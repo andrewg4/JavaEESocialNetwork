ea112b0f357c2e30f8c51a9514a40c69bf66d01f
Mygitcode <andrey.gulak94@gmail.com>
2015-06-29 15:46:30 +0300

    Created base project structure.

---
+++ src/main/java/net/simplesn/MainServlet.java
@@ -0,0 +1,26 @@
+package net.simplesn;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Andrew on 29.06.2015.
+ */
+
+@WebServlet("/mainservlet")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print("Hello SimpleSN");
+    }
+
+
+}
